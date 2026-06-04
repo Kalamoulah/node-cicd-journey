b10027d39a72c39f695feb4a11da8f4f00768cfb
kalamoulah <sagnam481@gmail.com>
2026-06-04 02:03:57 +0000

    udpade jenkinsfile

---
+++ src/app.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+  res.json({ message: 'Hello World' });
+});
+
+app.get('/health', (req, res) => {
+  res.json({ status: 'OK' });
+});
+
+module.exports = app;
